1a2151e8fd420406b6f9a64d35afc0c16e06a8fd
gflachs <Gregor.Flachs@student.htw-berlin.de>
2021-11-23 20:20:03 +0100

    Mockup

---
+++ src/main/java/mas/mockup/masMockup/MasMockupApplication.java
@@ -0,0 +1,13 @@
+package mas.mockup.masMockup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasMockupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasMockupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mas/mockup/masMockup/MasMockupApplicationTests.java
@@ -0,0 +1,13 @@
+package mas.mockup.masMockup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasMockupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
