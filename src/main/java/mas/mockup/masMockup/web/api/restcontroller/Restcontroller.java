825a7e6b1a7f9d05e662596ce39081f66209acab
gflachs <Gregor.Flachs@student.htw-berlin.de>
2021-12-07 19:10:25 +0100

    Mockup v0.8

--- src/main/java/mas/mockup/masMockup/web/api/restcontroller/Restcontroller.java
+++ src/main/java/mas/mockup/masMockup/web/api/restcontroller/Restcontroller.java
@@ -58,7 +58,7 @@ public class Restcontroller {
         this.orderService = orderService;
     }
 
-    @GetMapping(path = "api/v1/accounts/{email}")
+    @GetMapping(path = "/api/v1/accounts/{email}")
     ResponseEntity<AccountInfo> fetchAccountInfoByMail(@PathVariable(name = "email") String accountEmail) {
         AccountInfo info = accountInfoService.findByEmail(accountEmail);
         if (info == null) {
@@ -67,24 +67,24 @@ public class Restcontroller {
         return ResponseEntity.ok(info);
     }
 
-    @GetMapping(path = "api/v1/accounts/exists/{email}")
+    @GetMapping(path = "/api/v1/accounts/exists/{email}")
     ResponseEntity<Boolean> checkAccountExistens(@PathVariable(name = "email") String accountEmail) {
         return ResponseEntity.ok(accountInfoService.userExist(accountEmail));
     }
 
-    @PostMapping(path = "api/v1/accounts")
+    @PostMapping(path = "/api/v1/accounts")
     ResponseEntity<URI> createNewAccount(@RequestBody AccountInfoBody body) throws URISyntaxException {
         AccountInfo info = accountInfoService.createFromBody(body);
         URI uri = new URI("/api/v1/accounts/" + info.getAccountEmail());
         return ResponseEntity.created(uri).build();
     }
 
-    @GetMapping(path = "api/v1/accounts/{email}/orderAmount")
+    @GetMapping(path = "/api/v1/accounts/{email}/orderAmount")
     ResponseEntity<Integer> getAccountOrderAmount(@PathVariable(name = "email") String accountEmail) {
         return ResponseEntity.ok(accountInfoService.getOrderAmount(accountEmail));
     }
 
-    @GetMapping(path = "api/v1/supplierByMail/{email}")
+    @GetMapping(path = "/api/v1/supplierByMail/{email}")
     ResponseEntity<Supplier> fetchSupplierInfoByMail(@PathVariable(name = "email") String accountEmail) {
         Supplier info = supplierService.findByEmail(accountEmail);
         if (info == null) {
@@ -93,7 +93,7 @@ public class Restcontroller {
         return ResponseEntity.ok(info);
     }
 
-    @GetMapping(path = "api/v1/supplierById/{id}")
+    @GetMapping(path = "/api/v1/supplierById/{id}")
     ResponseEntity<Supplier> fetchSupplierInfoById(@PathVariable(name = "id") int id) {
         Supplier info = supplierService.findByID(id);
         if (info == null) {
@@ -102,25 +102,25 @@ public class Restcontroller {
         return ResponseEntity.ok(info);
     }
 
-    @GetMapping(path = "api/v1/supplier/exists/{email}")
+    @GetMapping(path = "/api/v1/supplier/exists/{email}")
     ResponseEntity<Boolean> checkSupplierExistens(@PathVariable(name = "email") String accountEmail) {
         return ResponseEntity.ok(supplierService.userExist(accountEmail));
     }
 
-    @PostMapping(path = "api/v1/supplier")
+    @PostMapping(path = "/api/v1/supplier")
     ResponseEntity<URI> createNewSupplier(@RequestBody SupplierBody body) throws URISyntaxException {
         Supplier info = supplierService.createFromBody(body);
         URI uri = new URI("/api/v1/supplier/" + info.getAccountEmail());
         return ResponseEntity.created(uri).build();
     }
 
-    @GetMapping(path = "api/v1/supplier/{id}/articles")
+    @GetMapping(path = "/api/v1/supplier/{id}/articles")
     ResponseEntity<Set<Article>> getSupplierArticles(@PathVariable(name = "id") int supplierID) {
         Set<Article> supplierArticles = supplierService.getSuppliersArticles(supplierID);
         return ResponseEntity.ok(supplierArticles);
     }
 
-    @GetMapping(path = "api/v1/article/{id}")
+    @GetMapping(path = "/api/v1/article/{id}")
     ResponseEntity<Article> fetchArticleById(@PathVariable(name = "id") int id) {
         Article article = articleService.findById(id);
         if (article == null) {
@@ -129,19 +129,19 @@ public class Restcontroller {
         return ResponseEntity.ok(article);
     }
 
-    @GetMapping(path = "api/v1/article")
+    @GetMapping(path = "/api/v1/article")
     ResponseEntity<List<Article>> fetchAllArticles() {
         return ResponseEntity.ok(articleService.findAll());
     }
 
-    @PostMapping(path = "api/v1/article")
+    @PostMapping(path = "/api/v1/article")
     ResponseEntity<URI> createArticle(@RequestBody ArticleBody body) throws URISyntaxException {
         Article article = articleService.createArticle(body);
         URI uri = new URI("api/v1/article/" + article.getArticleId());
         return ResponseEntity.created(uri).build();
     }
 
-    @GetMapping(path = "api/v1/offer/{id}")
+    @GetMapping(path = "/api/v1/offer/{id}")
     ResponseEntity<Offer> fetchOfferById(@PathVariable(name = "id") int id) {
         Offer offer = offerService.findById(id);
         if (offer == null) {
@@ -150,26 +150,26 @@ public class Restcontroller {
         return ResponseEntity.ok(offer);
     }
 
-    @GetMapping(path = "api/v1/offer")
+    @GetMapping(path = "/api/v1/offer")
     ResponseEntity<List<Offer>> fetchAllOffers() {
         return ResponseEntity.ok(offerService.findAll());
     }
 
-    @PostMapping(path = "api/v1/offer")
+    @PostMapping(path = "/api/v1/offer")
     ResponseEntity<URI> createOffer(@RequestBody OfferBody body) throws URISyntaxException {
         Offer offer = offerService.createOffer(body);
         URI uri = new URI("api/v1/offer/" + offer.getOfferID());
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping(path = "api/v1/offer/{id}")
+    @PutMapping(path = "/api/v1/offer/{id}")
     ResponseEntity<Offer> updateOfferStatus(@PathVariable(name = "id") int offerID,
             @RequestBody OfferStatus offerStatus) {
         Offer offer = offerService.updateOfferStatus(offerID, offerStatus);
         return offer == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(offer);
     }
 
-    @GetMapping(path = "api/v1/order/{id}")
+    @GetMapping(path = "/api/v1/order/{id}")
     ResponseEntity<Order> getOrderByID(@PathVariable(name = "id") int orderID) {
         Order order = orderService.findById(orderID);
         if (order == null) {
@@ -178,7 +178,7 @@ public class Restcontroller {
         return ResponseEntity.ok(order);
     }
 
-    @PostMapping(path = "api/v1/order")
+    @PostMapping(path = "/api/v1/order")
     ResponseEntity<URI> createNewOrder(@RequestBody OrderBody orderBody) throws URISyntaxException {
         Order order = orderService.createOrder(orderBody);
         Set<OrderLineItem> orderLineItems = new HashSet<>();
@@ -188,7 +188,7 @@ public class Restcontroller {
             orderLineItems.add(orderLineItemService.createOrderLineItem(body));
         }
         order.setOrderLineItems(orderLineItems);
-        URI uri = new URI("api/v1/order/" + order.getOrderID());
+        URI uri = new URI("/api/v1/order/" + order.getOrderID());
         return ResponseEntity.created(uri).build();
     }
 
